9fd9d4c88750f8323a5f9ac546d4b842396ea9ef
Vitor Silva Delfino <vitor.delfino952@gmail.com>
2017-08-29 20:54:38 -0300

    refactory

--- persistencia/connectionFactory.js
+++ persistencia/connectionFactory.js
@@ -3,20 +3,14 @@ var mysql = require('mysql');
 function createDBConnection() {
 
     if(!process.env.NODE_ENV){
-        /* return mysql.createConnection({
+        return mysql.createConnection({
             host: 'localhost',
             user: 'root',
             password: '123456',
             database: 'cabeleireiro',
             dateString: true
-        }); */
-        return mysql.createConnection({
-            host: 'us-cdbr-iron-east-05.cleardb.net',
-            user: 'b2d6f36bb81d89',
-            password: '39bf5014',
-            database: 'heroku_c4f53a955d2ee27',
-            dateString: true
         });
+       
     }
 
     if(process.env.NODE_ENV == 'test'){
