fafc89f6481aa6b787f67d711310db364478616c
vitor.delfino952 <vitor.delfino952@gmail.com>
2017-08-31 14:05:27 -0400

    login

--- persistencia/LoginDao.js
+++ persistencia/LoginDao.js
@@ -6,6 +6,10 @@ LoginDao.prototype.find = function(login, callback){
     this._connection.query('SELECT * FROM login where usuario = ? AND senha = ?', [login.usuario, login.senha], callback);
 }
 
+LoginDao.prototype.find2 = function(login, callback){
+    this._connection.query('SELECT * FROM login l, cliente c where usuario = ? AND senha = ? and l.usuario = cliente.cpf', [login.usuario, login.senha], callback);
+}
+
 module.exports = function() {
     return LoginDao;
 }
